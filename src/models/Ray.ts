d1ede5a0cb9edddec5e57e72ebe9dcd3f8fd575a
s4meb0dy <96389216+s4meb0dy@users.noreply.github.com>
2023-03-08 21:09:29 +0200

    create class Ray

---
+++ src/models/Ray.ts
@@ -0,0 +1,21 @@
+import Point from "./Point";
+import Vector from "./Vector";
+
+class Ray {
+    public origin : Point;
+    public direction: Vector;
+
+    constructor(origin: Point, direction : Vector) {
+        this.origin = origin;
+        this.direction = direction.Normalize();
+    }
+
+    public PointAt(t: number): Point {
+        const x = this.origin.x + t * this.direction.x;
+        const y = this.origin.y + t * this.direction.y;
+        const z = this.origin.z + t * this.direction.z;
+
+        return new Point(x,y,z);
+    }
+}
+export default Ray
