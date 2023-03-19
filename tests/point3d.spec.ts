8b8fe4d2017e685a25e70c8a8b8bc1846849dec7
s4meb0dy <96389216+s4meb0dy@users.noreply.github.com>
2023-03-19 19:52:34 +0200

    add tests for Ray, fix tests for point3D

--- tests/point3d.spec.ts
+++ tests/point3d.spec.ts
@@ -14,7 +14,7 @@ describe('Point3D', () => {
   });
 
   describe('toVector', () => {
-    it('returns a Vector3D object with the same values as the vertex', () => {
+    it('returns a Vector3D object with the same values as the point', () => {
       const vector = pointA.toVector();
       expect(vector.x).toBe(0);
       expect(vector.y).toBe(0);
@@ -25,17 +25,17 @@ describe('Point3D', () => {
   describe('isInsideRay', () => {
     const ray = new Ray(new Point3D(0, 0, 0), new Vector3D(1, 0, 0));
 
-    it('returns true for a vertex that is on the ray', () => {
+    it('returns true for a point that is on the ray', () => {
       const pointC = new Point3D(3, 0, 0);
       expect(pointC.isInsideRay(ray)).toBe(true);
     });
 
-    it('returns true for a vertex that is in front of the ray', () => {
+    it('returns true for a point that is in front of the ray', () => {
       const pointD = new Point3D(3, 1, 1);
       expect(pointD.isInsideRay(ray)).toBe(true);
     });
 
-    it('returns false for a vertex that is behind the ray', () => {
+    it('returns false for a point that is behind the ray', () => {
       const pointE = new Point3D(-1, 1, 1);
       expect(pointE.isInsideRay(ray)).toBe(false);
     });
