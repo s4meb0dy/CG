8ee17aaa5670b16b785d690e08f28c79e3059e4a
evgeniy-shpyra <foksmix256@gmail.com>
2023-03-23 01:46:30 +0200

    run app and fixed bugs

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
-import ConsoleRenderer from './ConsoleRenderer';
-import Camera from './objects/Camera';
-import { DirectionalLight } from './objects/Light';
-import { Sphere } from './objects/Sphere';
-import Vector3D from './objects/Vector3D';
-import Point3D from './objects/Point3D';
-import { Scene } from './types/Scene';
+import Camera from "./objects/Camera"
+import ConsoleRenderer from "./ConsoleRenderer"
+import { DirectionalLight } from "./objects/Light"
+import Plane from "./objects/Plane"
+import { Sphere } from "./objects/Sphere"
+import Vector3D from "./objects/Vector3D"
+import Point3D from "./objects/Point3D"
+import { Scene } from "./types/Scene"
 
 const camera = new Camera(
-  new Point3D(0, 0, -5),
-  new Vector3D(0, 0, 1),
-  1,
-  Math.PI / 3,
-  50
-);
+    new Point3D(0, 0, -5),
+    new Vector3D(0, 0, 1),
+    1,
+    Math.PI / 3,
+    50
+)
 
-const sphere = new Sphere(new Point3D(0, 0, 0), 1);
+const sphere = new Sphere(new Point3D(0, 0, 0), 1)
 
 
-
-const directionalLight = new DirectionalLight(new Vector3D(1, 0, 0));
+const directionalLight = new DirectionalLight(new Vector3D(1, 0, 0))
 
 const mainScene: Scene = {
-  camera,
-  objects: [sphere],
-  light: directionalLight,
-};
+    camera,
+    objects: [sphere],
+    light: directionalLight,
+}
 
-const consoleRenderer = new ConsoleRenderer(mainScene);
+const consoleRenderer = new ConsoleRenderer(mainScene)
 
-consoleRenderer.render();
+consoleRenderer.render()
