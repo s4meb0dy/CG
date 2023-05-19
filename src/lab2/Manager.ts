35baba948268ca6f20cc9c0d0bc502557260de67
evgeniy-shpyra <foksmix256@gmail.com>
2023-05-19 23:43:42 +0300

    fixed file formats checking

--- src/lab2/Manager.ts
+++ src/lab2/Manager.ts
@@ -1,82 +1,92 @@
-import fs from "fs";
-import ImageConverter from "./ImageConverter";
-import BMPReader from "./plugins/bmp/BMPReader";
-import BMPWriter from "./plugins/bmp/BMPWriter";
-import PPMReader from "./plugins/ppm/PPMReader";
-import PPMWriter from "./plugins/ppm/PPMWriter";
-import { error } from "console";
+import fs from "fs"
+import ImageConverter from "./ImageConverter"
+import BMPReader from "./plugins/bmp/BMPReader"
+import BMPWriter from "./plugins/bmp/BMPWriter"
+import PPMReader from "./plugins/ppm/PPMReader"
+import PPMWriter from "./plugins/ppm/PPMWriter"
 
 export default class Manager {
+    private inputFormat = ""
+    private outputFormat = ""
 
-    private inputFormat = ''
-    private outputFormat = ''
+    constructor(inputFormat: string, outputFormat: string) {
+        const directoryPath = "D:/projects/kpi/CG/src/lab2/plugins"
 
-    constructor(inputFormat: string, outputFormat: string){
-        if (fs.existsSync(inputFormat)){
-            throw new Error('Invalid input format');
-            
+        const files: string[] = fs.readdirSync(directoryPath)
+        if (!files.includes(outputFormat) || !files.includes(inputFormat)) {
+            const supported = files.join(", ")
+            throw new Error(
+                `Invalid input format. Supported only [${supported}] formats. :)`
+            )
         }
-        if (fs.existsSync(outputFormat)){
-            throw new Error('Invalid output format');
-            
-        }
+
         this.inputFormat = inputFormat
         this.outputFormat = outputFormat
     }
 
-    public getReadCallback (inputPath: string) {
-        switch(this.inputFormat){
-            case 'bmp':
+    public getReadCallback(inputPath: string) {
+        switch (this.inputFormat) {
+            case "bmp":
                 return () => {
                     return BMPReader.read(inputPath)
                 }
-                
-            case 'ppm':
+
+            case "ppm":
                 return () => {
                     return PPMReader.read(inputPath)
                 }
-            default: 
-                return () => {throw new Error('Invalid input format')}
+            default:
+                return () => {
+                    throw new Error("Invalid input format")
+                }
         }
     }
 
-    public getWriteCallback (outputPath: string, data: string | Buffer) {
-        switch(this.outputFormat){
-            case 'bmp':
+    public getWriteCallback(outputPath: string, data: string | Buffer) {
+        switch (this.outputFormat) {
+            case "bmp":
                 return () => {
-                    if(data instanceof Buffer)
+                    if (data instanceof Buffer)
                         return BMPWriter.write(outputPath, data)
-                    else () => {throw new Error('Some error')}
+                    else
+                        () => {
+                            throw new Error("Some error")
+                        }
                 }
-            case 'ppm':
+            case "ppm":
                 return () => {
-                    if(typeof data === 'string')
+                    if (typeof data === "string")
                         return PPMWriter.write(outputPath, data)
-                    else () => {throw new Error('Some error')}
+                    else
+                        () => {
+                            throw new Error("Some error")
+                        }
                 }
-            default: 
-                return () => {throw new Error('Some error')}
+            default:
+                return () => {
+                    throw new Error("Some error")
+                }
         }
     }
 
-    public getConvertorCallback (readFile: Buffer | string) {
-        switch(`${this.inputFormat} - ${this.outputFormat}`){
-            case 'bmp - ppm':
+    public getConvertorCallback(readFile: Buffer | string) {
+        switch (`${this.inputFormat} - ${this.outputFormat}`) {
+            case "bmp - ppm":
                 return () => {
-                    if(readFile instanceof Buffer)
+                    if (readFile instanceof Buffer)
                         return ImageConverter.BMPtoPPM(readFile)
-                    else throw new Error('Some error')
+                    else throw new Error("Some error")
                 }
-            case 'ppm - bmp':
+            case "ppm - bmp":
                 return () => {
-                    if(typeof readFile === 'string')
+                    if (typeof readFile === "string")
                         return ImageConverter.PPMtoBMP(readFile)
-                    else throw new Error('Some error')
+                    else throw new Error("Some error")
                 }
-            default: 
-                return () => {throw new Error('Invalid output format')}
+            default:
+                return () => {
+                    throw new Error("Invalid output format")
+                }
         }
-        
     }
-    
 }
--- src/lab2/index.ts
+++ src/lab2/index.ts
@@ -1,17 +1,17 @@
-import InputInformation from "./InputInformation";
-import Manager from "./Manager";
+import InputInformation from "./InputInformation"
+import Manager from "./Manager"
 
-
-try{
+try {
     const inputInformation = new InputInformation()
-    const manager = new Manager(inputInformation.inputFormat, inputInformation.outputFormat)
+    const manager = new Manager(
+        inputInformation.inputFormat,
+        inputInformation.outputFormat
+    )
 
     const readFile = manager.getReadCallback(inputInformation.inputPath)()
     const resultData = manager.getConvertorCallback(readFile)()
 
-    manager.getWriteCallback(inputInformation.outputPath, resultData)()       
-} 
-catch(error){
-    console.log("Error", error)
+    manager.getWriteCallback(inputInformation.outputPath, resultData)()
+} catch (error: any) {
+    console.log("Error", error?.message)
 }
-
