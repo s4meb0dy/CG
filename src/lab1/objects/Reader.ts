1691e93223ae984d04f437f15390a60357a90509
s4meb0dy <96389216+s4meb0dy@users.noreply.github.com>
2023-05-22 00:59:28 +0300

    add .obj Reader

---
+++ src/lab1/objects/Reader.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import Triangle from './Triangle';
+import Vector3D from './Vector3D';
+
+
+export default class Reader {
+  static readObjFile(filename: string): Triangle[] {
+
+    const data = fs.readFileSync(filename, 'utf-8');
+    const lines = data.split('\n');
+    const vertices: Vector3D[] = [];
+    const triangles: Triangle[] = [];
+
+    for (const line of lines) {
+      const tokens = line.trim().split(/\s+/);
+
+      if (tokens[0] === 'v') {
+        const x = parseFloat(tokens[1]);
+        const y = parseFloat(tokens[2]);
+        const z = parseFloat(tokens[3]);
+        vertices.push(new Vector3D(x, y, z));
+
+      } else if (tokens[0] === 'f') {
+        const vertexIndices: number[] = [];
+
+        for (let i = 1; i < tokens.length; i++) {
+          const vertexTokens = tokens[i].split('/');
+          const vertexIndex = parseInt(vertexTokens[0], 10) - 1;
+          vertexIndices.push(vertexIndex);
+        }
+
+        if (vertexIndices.length === 3) {
+          const v0 = vertices[vertexIndices[0]];
+          const v1 = vertices[vertexIndices[1]];
+          const v2 = vertices[vertexIndices[2]];
+          const triangle = new Triangle(v0, v1, v2);
+          triangles.push(triangle);
+        }
+      }
+    }
+
+    return triangles;
+  }
+}
---
+++ src/lab3/index.ts
@@ -0,0 +1,25 @@
+import Reader from "../lab1/objects/Reader"
+
+const start = async () => {
+    let src = ''
+
+    try {
+        const args = process.argv.slice(2)
+        args.forEach((arg) => {
+            if (arg.includes("--source=")) {
+                const inputFile = arg.slice(9)
+                
+                src = `${__dirname}/${inputFile}`.replace(/\\/g, "/")
+
+                const triangles = Reader.readObjFile(src);
+                console.log(triangles)
+            } 
+            else throw new Error('obj file error')
+        })
+    } catch (error: any) {
+        console.log("Error", error?.message)
+    }
+}
+
+start()
+
