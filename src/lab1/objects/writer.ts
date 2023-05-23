c6e9adc92316b413489d639e2bc3673e27eaa6f4
s4meb0dy <96389216+s4meb0dy@users.noreply.github.com>
2023-05-24 00:37:54 +0300

    add output to file and fix lab3

--- src/lab1/objects/writer.ts
+++ src/lab1/objects/writer.ts
@@ -1,3 +1,51 @@
-export default abstract class Writer {
-    abstract write() : void
+import PPMWriter from "../../lab2/plugins/ppm/PPMWriter";
+
+export default class Writer {
+  private imageData: number[][];
+  constructor(imageData: number[][]) {
+    this.imageData = imageData;
+  }
+
+  write(type: "console" | "file", path?: string ) {
+    if (type === "console") {
+      this.imageData.map((row) => {
+        row.map((v) => process.stdout.write(charFromScalarProduct(v)));
+        process.stdout.write("\n");
+      });
+    } else if (type === "file") {
+      this.imageData;
+      const header = `P3\n${this.imageData[0].length} ${this.imageData.length}\n255\n`;
+      const body = this.imageData
+        .map((row) =>
+          row
+            .map(
+              (v) =>
+                `${Math.round(255 * v)} ${Math.round(255 * v)} ${Math.round(
+                  255 * v
+                )}`
+            )
+            .join(" ")
+        )
+        .join("\n");
+
+      const fileData = `${header}${body}`;
+      if(path)
+        PPMWriter.write(path, fileData)
+      else console.log('You forgot add output path')
+    }
+
+    function charFromScalarProduct(scalarProduct: number): string {
+      if (scalarProduct < 0) {
+        return " ";
+      } else if (scalarProduct < 0.2) {
+        return ".";
+      } else if (scalarProduct < 0.5) {
+        return "*";
+      } else if (scalarProduct < 0.8) {
+        return "O";
+      } else {
+        return "#";
+      }
+    }
+  }
 }
--- src/lab3/index.ts
+++ src/lab3/index.ts
@@ -3,11 +3,10 @@ import Camera from "../lab1/objects/Camera";
 import Vector3D from "../lab1/objects/Vector3D";
 import Point3D from "../lab1/objects/Point3D";
 import Raytracer from "./rayTracer";
-import Triangle from "../lab1/objects/Triangle";
-// import Triangle from "./p/classes/triangle/Triangle";
 import {DirectionalLight} from "../lab1/objects/Light";
 import { MatrixTransformationEnum, MatrixTransformations, transformationFactory } from "./matrixTransformation";
-import WriterToConsole from "../lab1/objects/WriterToConsole";
+import Writer from "../lab1/objects/Writer";
+import { Sphere } from "../lab1/objects/Sphere";
 
 const start = async () => {
   let src = "";
@@ -23,49 +22,47 @@ const start = async () => {
       } else throw new Error("obj file error");
     });
 
-    const triangles = Reader.readObjFile(src);
-
+    
     const camera = new Camera(
-        new Point3D(0, 0, -50),
+        new Point3D(0, 0, -5000),
         new Vector3D(0, 0, 1),
         1,
         Math.PI / 3,
         100
       );
       const rayTracer = new Raytracer(camera, 100, 100);
-
+      
+      
+      const triangles = Reader.readObjFile(src);
       const objects = [
-        new Triangle(
-          new Vector3D(0, -150, 10),
-          new Vector3D(150, 150, 10),
-          new Vector3D(-150, 150, 10)
-        ),
-      ];
+        ...triangles,
+        new Sphere(new Vector3D(0, 400, -3000), 100)
+      ];      
 
-    // const lightDirection = new Vector3D(-0.5, -0.5, -1).normalize();
-    const lightDirection = new DirectionalLight(new Vector3D(-0.5, -0.5, -1))
+    const lightDirection = new DirectionalLight(new Vector3D(0, -0.5, -0.5))
 
     const matrixTransformations: MatrixTransformations[] = [
         {
           type: MatrixTransformationEnum.ROTATE,
           degrees: Math.PI / 45,
-          axis: new Vector3D(0, 0, 1),
+          axis: new Vector3D(0, 0, 0),
         },
         {
           type: MatrixTransformationEnum.SCALE,
-          scaleVector: new Vector3D(1, 1, 1.1),
+          scaleVector: new Vector3D(100, 150, 100),
         },
         {
           type: MatrixTransformationEnum.TRANSLATE,
-          translation: new Vector3D(-0.5, 0.5, -1),
+          translation: new Vector3D(1,2, 1),
         },
       ];
 
-      const imageData = rayTracer.trace(triangles, lightDirection.vector);
+      const imageData = rayTracer.trace(objects, lightDirection.vector, matrixTransformations);
       
-      const writer = new WriterToConsole(imageData)
-
-      writer.write();
+      const writer = new Writer(imageData)
+      
+      // writer.write('file', __dirname + '/output.ppm');
+      writer.write('console');
 
   } catch (error: any) {
     console.log("Error", error?.message);
